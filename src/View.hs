dbb68ec626c3a0bc58c99b49ce50af2d291ea905
Karl Voelker <ktvoelker@gmail.com>
2013-06-24 20:26:47 -0700

    Made the view thread and the main view loop

--- src/View.hs
+++ src/View.hs
@@ -1,57 +1,79 @@
 
+{-# LANGUAGE BangPatterns #-}
 module View where
 
 import Prelude hiding (FilePath)
 
-import Control.Concurrent.Chan.Strict
-import Control.DeepSeq
+import Control.Concurrent
 import Control.Monad
-import Control.Monad.Trans
+import Control.Monad.Reader
 import Control.Monad.Trans.Resource
 import qualified Data.Map as M
 import qualified Data.MeldableHeap as PQ
-import Data.Text
+import qualified Data.Text as T
 import Filesystem.Path
 import Graphics.Vty
 
 data ViewCommand =
-    AddResult !Integer !Text
-  | RemoveResult !Text
+    AddResult !Integer !T.Text
+  | RemoveResult !T.Text
   | ClearResults
+  | Shutdown
   deriving (Eq, Show)
-
-instance NFData ViewCommand
 
 newtype ViewHandle = ViewHandle { getChan :: Chan ViewCommand }
 
 sendViewCommand :: (MonadIO m) => ViewCommand -> ViewHandle -> m ()
-sendViewCommand cmd hnd = liftIO $ writeChan (getChan hnd) cmd
+sendViewCommand !cmd hnd = liftIO $ writeChan (getChan hnd) cmd
 
 data View =
   View
   { vOpts       :: ViewOptions
   , vResults    :: PQ.PQ (Integer, FilePath)
   , vResultsRev :: M.Map FilePath Integer
+  , vChan       :: Chan ViewCommand
   , vScreen     :: Vty
   }
 
 data ViewOptions =
   ViewOptions
-  { voQueryHandler  :: Text -> IO ()
+  { voQueryHandler  :: T.Text -> IO ()
   , voResultHandler :: FilePath -> IO ()
   }
 
--- TODO return a ViewHandle instead, and make sure the ResourceT can clean up
--- both the thread and the vty stuff
-newView :: (MonadResource m) => ViewOptions -> ResourceT m View
-newView opts = liftM (View opts PQ.empty M.empty . snd) $ allocate mkVty shutdown
+newView :: (MonadResource m) => ViewOptions -> ResourceT m ViewHandle
+newView opts = do
+  chan <- liftIO $ newChan
+  view <- liftM (View opts PQ.empty M.empty chan . snd) $ allocate mkVty shutdown
+  void $ liftM snd $ allocate (forkIO $ runView view) killThread
+  return $ ViewHandle chan
 
-addResult :: (MonadIO m) => Integer -> Text -> View -> m ()
+type M a = ReaderT View IO a
+
+runView :: View -> IO ()
+runView = runReaderT $ render >> viewLoop
+
+viewLoop :: M ()
+viewLoop =
+  asks vChan
+  >>= liftIO . getChanContents
+  >>= mapM_ runViewCommand . takeWhile (/= Shutdown)
+
+runViewCommand :: ViewCommand -> M ()
+runViewCommand (AddResult n xs) = addResult n xs
+runViewCommand (RemoveResult xs) = removeResult xs
+runViewCommand ClearResults = clearResults
+runViewCommand Shutdown = error "runViewCommand Shutdown"
+
+addResult :: Integer -> T.Text -> M ()
 addResult = undefined
 
-removeResult :: (MonadIO m) => Text -> View -> m ()
+removeResult :: T.Text -> M ()
 removeResult = undefined
 
-clearResults :: (MonadIO m) => View -> m ()
+clearResults :: M ()
 clearResults = undefined
+
+render :: M ()
+render = undefined
 
