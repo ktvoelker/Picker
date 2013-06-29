6e3a8b1df1ed5b8db88f5c73ffc6fa741f426244
Karl Voelker <ktvoelker@gmail.com>
2013-06-29 13:19:17 -0700

    Implemented the IO part of query evaluation

--- src/Query.hs
+++ src/Query.hs
@@ -3,16 +3,48 @@ module Query where
 
 import Prelude hiding (FilePath)
 
-import Control.Monad.Trans
+import Control.Monad.Reader
 import qualified Data.Text as T
-import Filesystem.Path ()
+import Filesystem
+import Filesystem.Path.CurrentOS
 
 import Query.Types
 
+data E = E { eQuery :: Query, eCallback :: T.Text -> IO () }
+
+type M = ReaderT E IO
+
 -- TODO
+evalDir' :: Query -> FilePath -> Bool
+evalDir' = undefined
+
+evalDir :: FilePath -> M ()
+evalDir fp = do
+  q <- asks eQuery
+  when (evalDir' q fp) $ liftIO (listDirectory fp) >>= mapM_ evalAny
+
+-- TODO
+evalFile' :: Query -> FilePath -> Bool
+evalFile' = undefined
+
+evalFile :: FilePath -> M ()
+evalFile fp = do
+  q <- asks eQuery
+  when (evalFile' q fp) $ asks eCallback >>= liftIO . ($ fp')
+  where
+    fp' = either (error "Unexpected encoding in a FilePath") id $ toText fp
+
+evalAny :: FilePath -> M ()
+evalAny fp = liftM2 (,) (liftIO $ isDirectory fp) (liftIO $ isFile fp) >>= \case
+  (True, False) -> evalDir fp
+  (False, True) -> evalFile fp
+  _             -> return ()
+
+runM :: Query -> (T.Text -> IO ()) -> M a -> IO a
+runM q cb m = runReaderT m (E q cb)
+
 evalQuery :: (MonadIO m) => Query -> (T.Text -> IO ()) -> m ()
-evalQuery (QText xs _) cb = liftIO $ mapM_ cb $ drop 1 $ T.inits xs
-evalQuery _ _ = undefined
+evalQuery q cb = liftIO $ getWorkingDirectory >>= runM q cb . evalAny
 
 -- TODO
 parseQuery :: T.Text -> Query
